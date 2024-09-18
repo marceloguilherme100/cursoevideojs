c204a1371c56dab0fcedf90ac50a3bc9988d8ec4
marceloguilherme100 <marcelo.guilherme100@gmail.com>
2024-09-18 14:00:38 -0300

    atualizar

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if  (vel > 60) { // condição simples !
+    consolelog('Voce ultrapassou a velocidade permetida. MULTAO!')
+}    
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ Aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 16
+if (idade <16){
+    console.log('Nao vota')
+}else if (idade < 18) {
+     console.log('Vota')
+}else {
+    console.log('Voto obrigatorio')
+}
